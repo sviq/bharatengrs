4c19937ff9158f834c07174f1b996dcf6b4b65b3
bhautik-sviq <bhautik@sviq.co.in>
2025-12-11 17:39:19 +0530

    fix mobile header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,15 +4,11 @@ import ClientsCorousel from '@/components/hero/ClientsCorousel';
 import CtaSection from '@/components/hero/Ctasection';
 import HeroSection from '@/components/hero/Hero';
 import ProductsSection from '@/components/hero/ProductsSection';
-import BannerLayout from '@/components/layouts/BannerLayout';
 
 export default function HomePage() {
   return (
     <main>
-      {/* <BannerLayout bannerImage="/banner/hero-banner.webp"> */}
-        <HeroSection />
-      {/* </BannerLayout> */}
-
+      <HeroSection />
       <AboutSection />
       <ProductsSection />
       <ClientsCorousel />
